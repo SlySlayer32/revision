170d6d48cedc32cf4fbe9201092705b75b4cb65c
SlySlayer32 <slyslayerfx@gmail.com>
2025-06-25 21:06:25 +1000

    auto-save: gemini_pipeline_cubit.dart at 2025-06-25 21:06:24

--- lib/features/ai_processing/presentation/cubit/gemini_pipeline_cubit.dart
+++ lib/features/ai_processing/presentation/cubit/gemini_pipeline_cubit.dart
@@ -2,7 +2,7 @@ import 'dart:typed_data';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:revision/core/services/gemini_pipeline_service.dart';
-import 'package:revision/features\ai_processing\domain\usecases\process_image_with_gemini_usecase.dart';
+import 'package:revision/features/ai_processing/domain/usecases/process_image_with_gemini_usecase.dart';
 
 /// Cubit for managing Gemini AI Pipeline state and operations
 /// 
