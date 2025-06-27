a78069f539c5fb64703715165a960ec2899cd27a
SlySlayer32 <slyslayerfx@gmail.com>
2025-06-27 13:49:57 +1000

    auto-save: firebase_options.dart at 2025-06-27 13:49:56

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -2,7 +2,7 @@
 // ignore_for_file: lines_longer_than_80_chars, avoid_classes_with_only_static_members
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
-    show TargetPlatform, defaultTargetPlatform, kIsWeb;
+    show defaultTargetPlatform, kIsWeb;
 
 // Import environment-specific configurations
 import 'firebase_options_dev.dart' as dev;
