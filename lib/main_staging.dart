9d1cf9db7d46539e967fdc0cb9fdcf25b0e14016
SlySlayer32 <slyslayerfx@gmail.com>
2025-06-28 16:53:02 +1000

    auto-save: main_staging.dart at 2025-06-28 16:53:02

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,26 +1,23 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:revision/app/app.dart';
 import 'package:revision/bootstrap.dart';
-import 'package:revision/core/di/service_locator.dart';
-import 'package:revision/firebase_options.dart';
+import 'package:revision/firebase_options_staging.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'revision-stg',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
-  try {
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-      name: 'revision-staging',
-    );
-
-    debugPrint('✅ Firebase initialized successfully for com.sly.revision.stg');
-  } catch (e) {
-    debugPrint('❌ Firebase initialization failed: $e');
-  }
-
-  // Initialize service locator
-  setupServiceLocator();
-
-  await bootstrap(() => const App());
+  bootstrap(App.new);
 }
