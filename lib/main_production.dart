58518b64b811979e8d3b6d345f4832dcbda62a10
SlySlayer32 <slyslayerfx@gmail.com>
2025-06-28 16:52:52 +1000

    auto-save: main_production.dart at 2025-06-28 16:52:52

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,6 +1,13 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:revision/app/app.dart';
 import 'package:revision/bootstrap.dart';
 import 'package:revision/core/di/service_locator.dart';
@@ -8,19 +15,16 @@ import 'package:revision/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  try {
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-    await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
-    debugPrint('✅ Firebase initialized successfully for com.sly.revision');
-  } catch (e) {
-    debugPrint('❌ Firebase initialization failed: $e');
-  }
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
 
   // Initialize service locator
   setupServiceLocator();
 
-  await bootstrap(() => const App());
+  // TODO(sly): Add Crashlytics
+  // TODO(sly): Add Analytics
+  // TODO(sly): Add Remote Config
+  bootstrap(() => const App());
 }
