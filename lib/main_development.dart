1ab176090a636b032badff4c9adb0506f8218345
SlySlayer32 <slyslayerfx@gmail.com>
2025-06-28 16:57:46 +1000

    auto-save: main_development.dart at 2025-06-28 16:57:46

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -3,16 +3,7 @@ import 'package:revision/app/app.dart';
 import 'package:revision/bootstrap.dart';
 
 void main() {
-  if (kDebugMode) {
-    print('main_development.dart: Starting bootstrap...');
-  }
   bootstrap(() {
-    if (kDebugMode) {
-      print('main_development.dart: Building App widget...');
-    }
     return App();
   });
-  if (kDebugMode) {
-    print('main_development.dart: App launched!');
-  }
 }
