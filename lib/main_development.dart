1dd4db8a275f79ff054037349cb0b7a0076ec5cb
SlySlayer32 <slyslayerfx@gmail.com>
2025-06-28 16:53:12 +1000

    auto-save: main_development.dart at 2025-06-28 16:53:12

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,13 +1,19 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:revision/app/app.dart';
 import 'package:revision/bootstrap.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  debugPrint('main_development.dart: Starting bootstrap...');
-  await bootstrap(() {
-    debugPrint('main_development.dart: Building App widget...');
+void main() {
+  if (kDebugMode) {
+    print('main_development.dart: Starting bootstrap...');
+  }
+  bootstrap(() {
+    if (kDebugMode) {
+      print('main_development.dart: Building App widget...');
+    }
     return const App();
   });
-  debugPrint('main_development.dart: App launched!');
+  if (kDebugMode) {
+    print('main_development.dart: App launched!');
+  }
 }
