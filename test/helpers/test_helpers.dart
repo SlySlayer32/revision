2e2157f5d8d708ce369f58e0b3551bcdb6bdb225
SlySlayer32 <slyslayerfx@gmail.com>
2025-06-30 20:56:07 +1000

    auto-save: test_helpers.dart at 2025-06-30 20:56:07

--- test/helpers/test_helpers.dart
+++ test/helpers/test_helpers.dart
@@ -1,10 +1,35 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:revision/core/di/service_locator.dart';
 import 'firebase_options_for_testing.dart';
 
-Future<void> setupFirebaseForTests() async {
-  TestWidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+class VGVTestHelper {
+  static Future<void> setupTestDependencies() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    // Reset the service locator to ensure a clean state for each test
+    getIt.reset();
+    // Setup the service locator with test dependencies
+    setupServiceLocator();
+  }
+
+  static Future<void> tearDownTestDependencies() async {
+    // Reset the service locator after each test
+    getIt.reset();
+  }
+
+  static Future<void> pumpAndSettle(
+    WidgetTester tester,
+    Widget widget,
+  ) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: widget,
+      ),
+    );
+    await tester.pumpAndSettle();
+  }
 }
