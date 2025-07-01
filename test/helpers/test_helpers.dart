5062451da3a7ba7f56297851df14780eb66cdd9c
SlySlayer32 <slyslayerfx@gmail.com>
2025-07-01 12:59:30 +1000

    auto-save: test_helpers.dart at 2025-07-01 12:59:30

--- test/helpers/test_helpers.dart
+++ test/helpers/test_helpers.dart
@@ -3,10 +3,15 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:revision/core/di/service_locator.dart';
 import 'firebase_options_for_testing.dart';
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
+import 'package:google_sign_in_mocks/google_sign_in_mocks.dart';
 
 class VGVTestHelper {
   static Future<void> setupTestDependencies() async {
     TestWidgetsFlutterBinding.ensureInitialized();
+    // Mock Firebase Core
+    final auth = MockFirebaseAuth();
+    final googleSignIn = MockGoogleSignIn();
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
