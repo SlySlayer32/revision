9f1ad0101f85e673da8c81e954acc52b9f0b9a8f
SlySlayer32 <slyslayerfx@gmail.com>
2025-07-01 13:02:01 +1000

    auto-save: test_helpers.dart at 2025-07-01 13:02:01

--- test/helpers/test_helpers.dart
+++ test/helpers/test_helpers.dart
@@ -2,51 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:revision/core/di/service_locator.dart';
-import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
-import 'package:flutter/services.dart';
-
-typedef Callback = void Function(MethodCall call);
-
-void setupFirebaseMocks([Callback? customHandlers]) {
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  TestDefaultBinaryMessenger.instance.setMockMethodCallHandler(
-      MethodChannelFirebase.channel, (call) async {
-    if (call.method == 'Firebase#initializeCore') {
-      return [
-        {
-          'name': defaultFirebaseAppName,
-          'options': {
-            'apiKey': '123',
-            'appId': '123',
-            'messagingSenderId': '123',
-            'projectId': '123',
-          },
-          'pluginConstants': {},
-        }
-      ];
-    }
-
-    if (call.method == 'Firebase#initializeApp') {
-      return {
-        'name': call.arguments['appName'],
-        'options': call.arguments['options'],
-        'pluginConstants': {},
-      };
-    }
-
-    if (customHandlers != null) {
-      customHandlers(call);
-    }
-
-    return null;
-  });
-}
+import 'firebase_options_for_testing.dart';
 
 class VGVTestHelper {
   static Future<void> setupTestDependencies() async {
-    setupFirebaseMocks();
-    await Firebase.initializeApp();
+    TestWidgetsFlutterBinding.ensureInitialized();
+    // Initialize a mock Firebase app
+    await Firebase.initializeApp(
+      name: 'test',
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
     // Reset the service locator to ensure a clean state for each test
     getIt.reset();
     // Setup the service locator with test dependencies
