8676f8f68c8159fc98cc65967e86afd54830068e
SlySlayer32 <slyslayerfx@gmail.com>
2025-07-02 12:12:24 +1000

    auto-save: gemini_ai_service.dart at 2025-07-02 12:12:24

--- lib/core/services/gemini_ai_service.dart
+++ lib/core/services/gemini_ai_service.dart
@@ -351,7 +351,7 @@ Please try again or contact support if the issue persists.
 
         final content = [
           Content.multi([
-            DataPart('image/jpeg', imageData),
+            new DataPart('image/jpeg', imageData),
             TextPart('''
 Describe this image in detail for photo editing purposes.
 
