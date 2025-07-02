3524779e29890f88712cb08fa1208a60b7d51f7e
SlySlayer32 <slyslayerfx@gmail.com>
2025-07-02 15:56:45 +1000

    auto-save: test_setup.dart at 2025-07-02 15:56:44

---
+++ test/helpers/test_setup.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:revision/core/di/service_locator.dart';
+import 'package:revision/helpers/firebase_test_helper.dart';
+
+Future<void> setupTestEnvironment() async {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  await FirebaseTestHelper.setupFirebaseForTesting();
+  setupServiceLocator();
+  await getIt.allReady();
+}
+
+Future<void> tearDownTestEnvironment() async {
+  await getIt.reset();
+}
