0af5ad33d1eb893e1ae29cb9c6a602acf238b20f
SlySlayer32 <slyslayerfx@gmail.com>
2025-07-02 18:43:56 +1000

    auto-save: firebase_helpers.dart at 2025-07-02 18:43:56

--- test/helpers/firebase_helpers.dart
+++ test/helpers/firebase_helpers.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockFirebaseAuth extends Mock implements FirebaseAuth {}
@@ -9,6 +10,6 @@ class MockFirebaseFirestore extends Mock implements FirebaseFirestore {}
 
 class MockFirebaseStorage extends Mock implements FirebaseStorage {}
 
-void setupFirebaseAuthMocks() {
-  // TODO: Implement mock setup
+Future<void> setupFirebaseAuthMocks() async {
+  TestWidgetsFlutterBinding.ensureInitialized();
 }
