e3e0696d610071c75e1f460a8a69a6bf4ef89692
SlySlayer32 <slyslayerfx@gmail.com>
2025-07-02 12:15:16 +1000

    auto-save: gemini_ai_service.dart at 2025-07-02 12:15:16

--- lib/core/services/gemini_ai_service.dart
+++ lib/core/services/gemini_ai_service.dart
@@ -3,7 +3,6 @@ import 'dart:developer';
 import 'dart:typed_data';
 
 import 'package:firebase_ai/firebase_ai.dart';
-import 'package:firebase_ai/src/google_ai.dart';
 import 'package:revision/core/constants/firebase_ai_constants.dart';
 import 'package:revision/core/services/ai_service.dart';
 import 'package:revision/core/services/firebase_ai_remote_config_service.dart';
