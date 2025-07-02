ff07ddf5c337717b1a0b34d169b2826f86fd55f6
SlySlayer32 <slyslayerfx@gmail.com>
2025-07-02 17:31:53 +1000

    auto-save: firebase_helpers.dart at 2025-07-02 17:31:53

---
+++ test/helpers/firebase_helpers.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.h';
+import 'package:mocktail/mocktail.dart';
+
+class MockFirebaseAuth extends Mock implements FirebaseAuth {}
+
+class MockFirebaseFirestore extends Mock implements FirebaseFirestore {}
+
+class MockFirebaseStorage extends Mock implements FirebaseStorage {}
