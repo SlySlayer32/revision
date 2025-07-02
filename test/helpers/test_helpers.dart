2e911381a13f48d216c93e725703c70afae0631d
SlySlayer32 <slyslayerfx@gmail.com>
2025-07-02 17:30:52 +1000

    auto-save: test_helpers.dart at 2025-07-02 17:30:52

--- test/helpers/test_helpers.dart
+++ test/helpers/test_helpers.dart
@@ -1,4 +1,4 @@
-import 'package:mockito/mockito.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:revision/features/image_editor/data/services/image_save_service.dart';
 
 class MockImageSaveService extends Mock implements ImageSaveService {}
