6be6cb951a2173bf424eb2edeb4640b5b8b4e38c
SlySlayer32 <slyslayerfx@gmail.com>
2025-07-02 18:42:51 +1000

    auto-save: helpers.dart at 2025-07-02 18:42:51

--- test/helpers/helpers.dart
+++ test/helpers/helpers.dart
@@ -1,9 +1,2 @@
-// Core test utilities
-export 'firebase_auth_helper.dart';
-export 'firebase_emulator_helper.dart';
-export 'golden_test_helper.dart';
-export 'integration_test_helper.dart';
-// VGV test infrastructure
-export 'mocks.dart';
-export 'pump_app.dart';
-export 'test_data_factory.dart';
+export 'test_helpers.dart';
+export 'firebase_helpers.dart';
