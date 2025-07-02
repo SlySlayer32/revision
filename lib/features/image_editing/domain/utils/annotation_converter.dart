77bb20bf725502c8597d7a10a84148736fa601a1
SlySlayer32 <slyslayerfx@gmail.com>
2025-07-02 18:36:48 +1000

    auto-save: annotation_converter.dart at 2025-07-02 18:36:48

--- lib/features/image_editing/domain/utils/annotation_converter.dart
+++ lib/features/image_editing/domain/utils/annotation_converter.dart
@@ -15,4 +15,25 @@ class AnnotationConverter {
       };
     }).toList();
   }
+
+  static String generatePromptFromAnnotations(
+      List<AnnotationStroke> annotations, String userPrompt) {
+    if (annotations.isEmpty) {
+      return userPrompt;
+    }
+
+    final promptParts = [userPrompt];
+    promptParts.add('The user has marked the following areas:');
+
+    for (var i = 0; i < annotations.length; i++) {
+      final stroke = annotations[i];
+      final color = stroke.color;
+      final colorName =
+          '#${color.value.toRadixString(16).substring(2).toUpperCase()}';
+      promptParts.add(
+          '  - Area ${i + 1}: A stroke with color $colorName and width ${stroke.strokeWidth.toStringAsFixed(2)}');
+    }
+
+    return promptParts.join('\n');
+  }
 }
