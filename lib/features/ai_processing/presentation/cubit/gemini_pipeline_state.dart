703158f8313f5e8a40824a14dfd9f862daa63148
SlySlayer32 <slyslayerfx@gmail.com>
2025-07-03 01:06:02 +1000

    auto-save: gemini_pipeline_state.dart at 2025-07-03 01:06:02

---
+++ lib/features/ai_processing/presentation/cubit/gemini_pipeline_state.dart
@@ -0,0 +1,42 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+import 'package:revision/features/ai_processing/domain/entities/processing_result.dart';
+
+enum GeminiPipelineStatus {
+  initial,
+  processing,
+  success,
+  error,
+  cancelled,
+}
+
+class GeminiPipelineState extends Equatable {
+  const GeminiPipelineState({
+    this.status = GeminiPipelineStatus.initial,
+    this.processingResult,
+    this.errorMessage,
+    this.progressMessage,
+  });
+
+  final GeminiPipelineStatus status;
+  final ProcessingResult? processingResult;
+  final String? errorMessage;
+  final String? progressMessage;
+
+  GeminiPipelineState copyWith({
+    GeminiPipelineStatus? status,
+    ProcessingResult? processingResult,
+    String? errorMessage,
+    String? progressMessage,
+  }) {
+    return GeminiPipelineState(
+      status: status ?? this.status,
+      processingResult: processingResult ?? this.processingResult,
+      errorMessage: errorMessage ?? this.errorMessage,
+      progressMessage: progressMessage ?? this.progressMessage,
+    );
+  }
+
+  @override
+  List<Object?> get props => [status, processingResult, errorMessage, progressMessage];
+}
