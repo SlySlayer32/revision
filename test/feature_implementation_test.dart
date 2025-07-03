a28a14c5a7c5e44ebc508c4fcc7c5e2fb34d2452
SlySlayer32 <slyslayerfx@gmail.com>
2025-07-03 18:06:50 +1000

    auto-save: feature_implementation_test.dart at 2025-07-03 18:06:50

--- test/feature_implementation_test.dart
+++ test/feature_implementation_test.dart
@@ -129,10 +129,10 @@ void main() {
         final imageBytes = kTestPng;
         final markedAreas = [
           const MarkedArea(
-            x: 10.0,
-            y: 20.0,
-            width: 30.0,
-            height: 40.0,
+            x: 0.1,
+            y: 0.1,
+            width: 0.2,
+            height: 0.2,
             description: 'unwanted object',
           ),
         ];
