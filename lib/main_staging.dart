e02a3e5ab93d3580fa8bba01bc0c6e0488e4b361
SlySlayer32 <slyslayerfx@gmail.com>
2025-07-05 04:13:57 +1000

    auto-save: main_staging.dart at 2025-07-05 04:13:57

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,23 +1,10 @@
-// Copyright (c) 2022, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:revision/app/app.dart';
 import 'package:revision/bootstrap.dart';
-import 'package:revision/firebase_options_staging.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    name: 'revision-stg',
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  bootstrap(App.new);
+  await dotenv.load(fileName: ".env.staging");
+  bootstrap(() => const App());
 }
