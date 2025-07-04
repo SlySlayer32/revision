6451b9f7908ba5895fa4cd71390f74b6366a13d3
SlySlayer32 <slyslayerfx@gmail.com>
2025-07-05 04:13:31 +1000

    auto-save: main_development.dart at 2025-07-05 04:13:31

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,8 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:revision/app/app.dart';
 import 'package:revision/bootstrap.dart';
+import 'package:revision/core/config/environment_detector.dart';
 
-void main() {
-  bootstrap(() {
-    return const App();
-  });
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env.development");
+  EnvironmentDetector.initialize(AppEnvironment.development);
+  bootstrap(() => const App());
 }
