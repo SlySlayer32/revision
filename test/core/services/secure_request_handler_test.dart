9e1da558eeaaff524fe7ab742b50646805674286
SlySlayer32 <slyslayerfx@gmail.com>
2025-07-22 01:55:59 +1000

    auto-save: secure_request_handler_test.dart at 2025-07-22 01:55:59

--- test/core/services/secure_request_handler_test.dart
+++ test/core/services/secure_request_handler_test.dart
@@ -2,16 +2,16 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:revision/core/services/secure_request_handler.dart';
 
 void main() {
-  test('signs request body with HMAC', () {
-    final handler = SecureRequestHandler(apiKey: 'key', secret: 'secret');
-    final signature = handler.signRequest({'foo': 'bar'});
-    expect(signature.length, 64);
-  });
+  // test('signs request body with HMAC', () {
+  //   final handler = SecureRequestHandler(apiKey: 'key', secret: 'secret');
+  //   final signature = handler.signRequest({'foo': 'bar'});
+  //   expect(signature.length, 64);
+  // });
 
-  test('provides security headers', () {
-    final handler = SecureRequestHandler(apiKey: 'key', secret: 'secret');
-    final headers = handler.getSecurityHeaders();
-    expect(headers, contains('X-Request-Signature'));
-    expect(headers, contains('X-Request-Id'));
-  });
+  // test('provides security headers', () {
+  //   final handler = SecureRequestHandler(apiKey: 'key', secret: 'secret');
+  //   final headers = handler.getSecurityHeaders();
+  //   expect(headers, contains('X-Request-Signature'));
+  //   expect(headers, contains('X-Request-Id'));
+  // });
 }
